fe5671dfbd617101ac9909e930a30d22c0284d42
evan.yu <evan.yu@dji.com>
2016-12-07 14:35:04 +0800

    add silverseacher

---
+++ silversearcher.sh
@@ -0,0 +1,7 @@
+##ubuntu install
+sudo add-apt-repository ppa:pgolm/the-silver-searcher
+sudo apt-get update
+#sudo apt-get install the-silver-searcher
+sudo apt-get install silversearcher-ag
+##mac install
+brew install silver-searcher
